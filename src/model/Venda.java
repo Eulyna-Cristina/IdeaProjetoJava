9b290a68248953ca0bae859ce98b71d26e1bd51c
Lenilson <lenilsonpereira777@gmail.com>
2025-11-23 08:25:00 -0300

    Criação da classe nota fiscal, alteração na Main e criação da classe venda.

---
+++ src/model/Venda.java
@@ -0,0 +1,45 @@
+package model;
+
+public class Venda {
+
+    private int id;
+    private Aluno aluno;
+    private Curso curso;
+    private double valor;
+    private String formaPg;
+
+    
+    public Venda(int id, Aluno aluno, Curso curso, String formapg) {
+        this.id = id;
+        this.aluno = aluno;
+        this.curso = curso;
+        this.valor = curso.getPreco();
+        this.formaPg = formapg;
+    }
+
+    // Getters (mantidos como estavam)
+    public int getId() {
+        return id;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public double getPreco() {
+        return valor;
+    }
+
+    public String getFormadePagamento() {
+        return formaPg;
+
+
+
+
+
+    }
+}
